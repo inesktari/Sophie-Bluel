f842be0525cc7683503b0abebbc58acfed390657
inesktari <135965289+inesktari@users.noreply.github.com>
2024-04-17 14:32:40 +0200

    Login user

---
+++ FrontEnd/assets/login.js
@@ -0,0 +1,35 @@
+const login_API = "http://localhost:5678/api/users/login";
+const login_button = document.getElementById("login");
+
+login_button.addEventListener("submit", function (event) {
+  event.preventDefault();
+  loginUser();
+});
+
+function loginUser() {
+  let user = {
+    email: document.getElementById("email").value,
+    password: document.getElementById("password").value,
+  };
+
+  fetch(login_API, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(user),
+  })
+    .then((response) => {
+      if (response.status === 200) {
+        return response.json();
+      } else {
+        loginError = document.getElementById("login_error");
+        loginError.innerHTML =
+          "E-mail et/ou mot de passe incorrects. Veuillez réessayer";
+      }
+    })
+    .then((data) => {
+      if (data) {
+        window.localStorage.setItem("token", "my_token");
+        window.location.href = "index.html";
+      }
+    });
+}
